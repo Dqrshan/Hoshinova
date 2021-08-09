0c05399911740fdff6f123784eb935bc7394045b
Nin <darshan310268@gmail.com>
2021-08-09 13:34:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = client;
 
 client.commands = new Collection();
 client.slashCommands = new Collection();
+client.snipes = new Collection();
 client.config = require("./config.json");
 
 require("./handler")(client);
